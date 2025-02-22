234985aa83c7c4c2fa6605d399eb3347f48c7dc1
rodrigobrocchi <rodrigo.brocchi@gmail.com>
2025-02-22 01:56:57 -0300

    [✨ feat](README.md): Add Docker instructions for building and running the application.

--- src/main/java/br/com/imaginer/resqueueuser/adapter/controller/KeycloakGetUserController.java
+++ src/main/java/br/com/imaginer/resqueueuser/adapter/controller/KeycloakGetUserController.java
@@ -5,10 +5,16 @@ import br.com.imaginer.resqueueuser.adapter.gateway.keycloak.getuser.GetUserServ
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
+@Slf4j
 @Tag(name = "Keycloak User Details")
 @RestController
 @RequestMapping("/users")
@@ -22,7 +28,16 @@ public class KeycloakGetUserController {
 
   @Operation(description = "Get user details by email.", security = { @SecurityRequirement(name = "bearer-key") })
   @GetMapping
-  public Mono<ResponseEntity<GetUserResponse>> getUser(@RequestParam String email) {
+  public Mono<ResponseEntity<GetUserResponse>> getUser(
+      @RequestParam String email,
+      @AuthenticationPrincipal Jwt jwt
+  ) {
+
+    UUID userId = UUID.fromString(jwt.getSubject());
+    String userEmailFromToken = jwt.getClaim("email");
+
+    log.info("Usuário autenticado: " + userId + " - " + userEmailFromToken);
+
     return getUserService.getUser(email)
         .map(ResponseEntity::ok);
   }
